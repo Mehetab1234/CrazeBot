01f584908f7a1f3600dc998addc542949d3b6850
CRAFTINGCRAZEGAMIMG <mehetabhoque19@gmail.com>
2025-04-21 18:31:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const fs = require('fs');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.commands = new Collection();
+
+const commandFolders = fs.readdirSync('./commands');
+for (const folder of commandFolders) {
+  const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+  for (const file of commandFiles) {
+    const command = require(`./commands/${folder}/${file}`);
+    client.commands.set(command.data.name, command);
+  }
+}
+
+client.once('ready', () => {
+  console.log(`Bot is online as ${client.user.tag}`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({ content: 'There was an error executing that command.', ephemeral: true });
+  }
+});
+
+client.login(process.env.TOKEN);
